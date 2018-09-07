bfffba654269a5a482eaab22cb130d16d2cc73f9
inoyyth <supri170845@gmail.com>
2018-09-07 07:55:35 +0700

    initial master file

---
+++ database/migrations/2018_06_24_070946_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    //protected $table = 'custom_products';
+    protected $fillable = ['title', 'description', 'price', 'availability'];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('price');
+            $table->boolean('availability');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
